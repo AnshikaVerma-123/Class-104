c629faa4f3fea476c759e7bb479a3db1dcab60ad
AnshikaVerma-123 <73770741+AnshikaVerma-123@users.noreply.github.com>
2021-06-08 19:02:51 +0530

    Add files via upload

---
+++ statistics.py
@@ -0,0 +1,12 @@
+import csv 
+#with open ('SOCR-HeightWeight.csv')as f:
+# reader= csv.reader(f)
+#file_data= list(reader)
+from collections import Counter
+new_data ="whitehatJr" 
+data= Counter(new_data)
+#print(data)
+#new_list = data.items()
+#print(new_list)
+new_list = data.values()
+print(new_list)
